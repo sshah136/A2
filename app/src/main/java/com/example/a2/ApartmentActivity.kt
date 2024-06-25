73e029bbeb3ef22462f4bfeab25a3604a13bbc13
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-25 18:01:53 -0400

    Populate Apt Listings

--- app/src/main/java/com/example/a2/ApartmentActivity.kt
+++ app/src/main/java/com/example/a2/ApartmentActivity.kt
@@ -2,8 +2,13 @@ package com.example.a2
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,7 +24,31 @@ class ApartmentActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Populate linear layout with data
+        val listings = listOf<Listing>(
+            Listing("123 Main St", "$200,000", R.drawable.homepl),
+            Listing("456 Elm St", "$180,000", R.drawable.homepl),
+            Listing("789 Oak St", "$220,000", R.drawable.homepl)
+        )
+        val linearLayoutHomes = findViewById<LinearLayout>(R.id.llApt)
+
+        for (listing in listings) {
+            val view = LayoutInflater.from(this).inflate(R.layout.item_home, linearLayoutHomes, false)
+
+            val checkBox = view.findViewById<CheckBox>(R.id.checkBox)
+            val imageView = view.findViewById<ImageView>(R.id.ivHome)
+            val tvAddress = view.findViewById<TextView>(R.id.tvAddress)
+            val tvPrice = view.findViewById<TextView>(R.id.tvPrice)
+
+            imageView.setImageResource(listing.image)
+            tvAddress.text = listing.title
+            tvPrice.text = listing.price
+
+            linearLayoutHomes.addView(view)
+        }
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.app_bar_menu, menu)
