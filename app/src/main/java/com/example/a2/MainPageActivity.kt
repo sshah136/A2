0286401e3a876cd33d7deba2bbdb73622dee9d21
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-25 16:53:56 -0400

    btnEnter Click Listener

--- app/src/main/java/com/example/a2/MainPageActivity.kt
+++ app/src/main/java/com/example/a2/MainPageActivity.kt
@@ -1,6 +1,8 @@
 package com.example.a2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainPageActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnEnter = findViewById<Button>(R.id.btnEnter)
+
+        btnEnter.setOnClickListener {
+            val intent = Intent(this, SelectHomeTypeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
