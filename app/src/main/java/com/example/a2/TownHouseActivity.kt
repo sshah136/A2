713f706c6aa7cac0bbaf6ca6708634763eeb0bc9
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-25 16:55:44 -0400

    Added Menu Bar Navigation

--- app/src/main/java/com/example/a2/TownHouseActivity.kt
+++ app/src/main/java/com/example/a2/TownHouseActivity.kt
@@ -1,6 +1,9 @@
 package com.example.a2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,31 @@ class TownHouseActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.miHome -> {
+                startActivity(Intent(this, DetachedHouseActivity::class.java))
+            }
+            R.id.miSemiHome -> {
+                startActivity(Intent(this, SemiDetachedActivity::class.java))
+            }
+            R.id.miCondo -> {
+                startActivity(Intent(this, CondoActivity::class.java))
+            }
+            R.id.miApt -> {
+                startActivity(Intent(this, ApartmentActivity::class.java))
+            }
+            R.id.miTownHome -> {
+                startActivity(Intent(this, TownHouseActivity::class.java))
+            }
+        }
+        return true
     }
 }
