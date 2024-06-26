8742771d7363f199c3f070d5eb68ec4c7ef5db6a
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-26 00:38:57 -0400

    Created Activity and layouts

---
+++ app/src/main/java/com/example/a2/PaymentActivity.kt
@@ -0,0 +1,28 @@
+package com.example.a2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_payment)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnPlaceOrder = findViewById<Button>(R.id.btnPlaceOrder)
+        btnPlaceOrder.setOnClickListener {
+            val intent = Intent(this, ConfirmationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
