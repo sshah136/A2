5ede6be90af16cc7f857fa37f757675b7d697073
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-26 00:01:59 -0400

    Create CheckoutActivity.kt

---
+++ app/src/main/java/com/example/a2/CheckoutActivity.kt
@@ -0,0 +1,51 @@
+package com.example.a2
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.gson.Gson
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_checkout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val sharedPref = getSharedPreferences("MyPrefs", Context.MODE_APPEND)
+        val getSelectedListings = sharedPref.getString("selected_listings", null)
+        val selectedListings = if (getSelectedListings != null) {
+            Gson().fromJson(getSelectedListings, Array<Listing>::class.java).toList()
+        } else {
+            emptyList()
+        }
+        val llCheckOut = findViewById<LinearLayout>(R.id.llCheckOut)
+
+        for (listing in selectedListings) {
+            val view = LayoutInflater.from(this).inflate(R.layout.item_home_checkout, llCheckOut, false)
+
+            //val checkBox = view.findViewById<CheckBox>(R.id.checkBox)
+            val imageView = view.findViewById<ImageView>(R.id.ivHome)
+            val tvAddress = view.findViewById<TextView>(R.id.tvAddress)
+            val tvPrice = view.findViewById<TextView>(R.id.tvPrice)
+
+            imageView.setImageResource(listing.image)
+            tvAddress.text = listing.title
+            tvPrice.text = listing.price
+
+            llCheckOut.addView(view)
+        }
+    }
+}
