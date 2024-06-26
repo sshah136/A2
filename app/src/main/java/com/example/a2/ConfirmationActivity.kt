5c8dcd72265d88a131a8a9f2713053d31217fc0b
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-26 02:05:17 -0400

    Retrieve Shared Preference from Checkout Activity

--- app/src/main/java/com/example/a2/ConfirmationActivity.kt
+++ app/src/main/java/com/example/a2/ConfirmationActivity.kt
@@ -1,12 +1,21 @@
 package com.example.a2
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.gson.Gson
 
 class ConfirmationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,28 @@ class ConfirmationActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val sharedPreferences = getSharedPreferences("CheckOutPref", MODE_PRIVATE)
+        val selectedListingJson = sharedPreferences.getString("selected_listing", null)
+        Log.d("ConfirmationActivity", "JSON from SharedPreferences: $selectedListingJson")
+        val selectedListing = Gson().fromJson(selectedListingJson, Listing::class.java)
+        Log.d("ConfirmationActivity", "JSON from SharedPreferences: $selectedListing")
+        val llOrderDetails = findViewById<LinearLayout>(R.id.llOrderDetails)
+
+
+        val view = LayoutInflater.from(this)
+            .inflate(R.layout.item_home_checkout, llOrderDetails, false)
+
+        val tvAddress = view.findViewById<TextView>(R.id.tvAddress)
+        val tvPrice = view.findViewById<TextView>(R.id.tvPrice)
+        val imageView = view.findViewById<ImageView>(R.id.ivHome)
+
+        tvAddress.text = selectedListing.title
+        tvPrice.text = selectedListing.price
+        imageView.setImageResource(selectedListing.image)
+
+
+        llOrderDetails.addView(view)
     }
 }
+
