56f074d60af8b3646f0d4bcf6f46950dd2e3765c
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-26 00:01:46 -0400

    Added Gson implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.code.gson:gson:2.8.8")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
