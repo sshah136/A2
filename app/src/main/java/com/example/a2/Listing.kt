b2d8e3cc60f321ac3093eb961421108f0e587661
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-26 02:05:23 -0400

    Update Listing.kt

--- app/src/main/java/com/example/a2/Listing.kt
+++ app/src/main/java/com/example/a2/Listing.kt
@@ -3,6 +3,6 @@ package com.example.a2
 data class Listing (
     val title: String,
     val price: String,
-    val image: Int,
-    var isSelected: Boolean = false
+    val image: Int
+    //var isSelected: Boolean = false
 )
